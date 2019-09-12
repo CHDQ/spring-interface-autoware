26fd00b6f7b935fc0f3c9e8ab8ef167024997198
CHDQ <hh934609672@163.com>
2019-09-12 16:46:48 +0800

    second

--- src/main/java/org/dq/test/ex/framework/HttpProxyHandler.java
+++ src/main/java/org/dq/test/ex/framework/HttpProxyHandler.java
@@ -7,14 +7,14 @@ import java.net.http.HttpClient;
 public class HttpProxyHandler implements InvocationHandler {
     private String url;
 
-    public HttpProxyHandler(String url) {
+    HttpProxyHandler(String url) {
         this.url = url;
     }
 
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
         HttpRequest annotation = method.getAnnotation(HttpRequest.class);
-        System.out.println(url + annotation.value());
+        System.out.println(url + annotation.value());//这里没有做多余操作，可以在该处远程调用等操作
         return null;
     }
 }
